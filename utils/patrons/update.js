0c47a9be271a0dbd9e6cc77472ea98c007a1d826
Alessandro Pepe <apepe1024@gmail.com>
2018-05-18 21:35:57 -0400

    second review bug fix; updating patron should not result in a 500 error now

--- utils/patrons/update.js
+++ utils/patrons/update.js
@@ -15,29 +15,35 @@ module.exports = (req, res, next) => {
       res.redirect('/patrons/' + patron.id);
     }).catch((err) => {
       if (err.name === 'SequelizeValidationError') {
-        loans.findAll({ include: [{ model: books }, { model: patrons, where: { id: req.params.id } }] })
-          .then((results) => {
-            if (results) {
+        patrons.findById(req.params.id, {})
+          .then((patron) => {
+            if (patron) {
               let patronObject = {};
-              let getLoans = JSON.parse(JSON.stringify(results));
 
               patronObject = {
-                id: getLoans[0].patron.id,
-                first_name: getLoans[0].patron.first_name,
-                last_name: getLoans[0].patron.last_name,
-                address: getLoans[0].patron.address,
-                email: getLoans[0].patron.email,
-                library_id: getLoans[0].patron.library_id,
-                zip_code: getLoans[0].patron.zip_code
+                id: patron.id,
+                first_name: patron.first_name,
+                last_name: patron.last_name,
+                address: patron.address,
+                email: patron.email,
+                library_id: patron.library_id,
+                zip_code: patron.zip_code
               };
 
-              res.render('partials/patron_details', {
-                patron: patronObject,
-                results: results,
-                title: getLoans[0].patron.first_name + ' ' + getLoans[0].patron.last_name,
-                errors: err.errors
-              });
-            }
+              let loanCheck = function (patron) {
+                loans.findAll({ include: [{ model: books, attributes: ['id', 'title'] }, { model: patrons, where: { id: req.params.id }, attributes: ['first_name', 'last_name'] }] })
+                  .then((results) => {
+                    res.render('partials/patron_details', {
+                      patron: patronObject,
+                      results: results,
+                      title: patron.first_name + ' ' + patron.last_name,
+                      errors: err.errors
+                    });
+                  });
+              };
+
+              loanCheck(patron);
+            };
           }).catch((err) => {
             console.log(err);
             res.sendStatus(500);
